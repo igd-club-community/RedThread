dad3a6ce5caa6e1c8319b64a238b20e77f8a7d82
Смолев Виталий <smolev.vitaliy@gmail.com>
2019-02-04 23:18:55 +0300

    Added persons scripts and level1

---
+++ Assets/Scripts/Persons/ActingPerson.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ActingPerson : MonoBehaviour
+{
+    public PersonAct[] actions;
+    public PersonAct currentAction;
+    public bool noAction = false;
+    int curActNum = 0;
+    public float distance = 0;
+    NavMeshAgent navAgent;
+
+    //У персоны есть его целевая позиция куда он хочет идти
+    //Есть базовый цикл из задач которые он делает пока ничего не происходит
+    //У каждой отдельной персоны будет свой цикл.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAction = actions[curActNum];
+        navAgent = GetComponent<NavMeshAgent>();
+        setAction(curActNum);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(currentAction.target.position, transform.position);
+        if (distance < 1)
+        {
+            curActNum = (curActNum + 1) % actions.Length;
+        }
+        setAction(curActNum);
+    }
+
+    private void setAction(int curActNum)
+    {
+        currentAction = actions[curActNum];
+        navAgent.SetDestination(currentAction.target.position);
+    }
+}
+
+public enum PersonState
+{
+    Idle,
+    Acting
+}
---
+++ Assets/Scripts/Persons/PersonAct.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PersonAct
+{
+    public Transform target;
+    public string animationName; //TODO: can be changed to think that convinient to describe animation
+}
