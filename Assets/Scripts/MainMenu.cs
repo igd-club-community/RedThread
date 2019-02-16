1b4b8b4c8878f1fc07630709feec232edf48e4a5
Смолев Виталий <smolev.vitaliy@gmail.com>
2019-02-16 18:39:28 +0300

    Added visible UI

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,16 +5,4 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Play the game scene.
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-    // Quit the game application. 
-    public void QuitGame()
-    {
-        Debug.Log("Quit!");
-        Application.Quit();
-    }
 }
