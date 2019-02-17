1b8a07eaecd65d19c7482f9ab0559685a9a1d346
oninbo <oninbo@gmail.com>
2019-02-17 21:57:04 +0300

    added inventory items drag

---
+++ Assets/Scripts/ItemIconDrag.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemIconDrag : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    public void OnDrag(PointerEventData data)
+    {
+        transform.position = data.pointerCurrentRaycast.screenPosition;
+    }
+
+    public void OnEndDrag(PointerEventData data)
+    {
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = new Vector3(0, 0, 0);
+    }
+}
