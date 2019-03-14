5a8e8658ee330af4d184ae0f2956209ab0a0ce22
Apostrov <amir.subaev@gmail.com>
2019-03-14 20:14:14 +0300

    Journal Hide If Clicked Outside

--- Assets/Scripts/UI/JournalPaper.cs
+++ Assets/Scripts/UI/JournalPaper.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class JournalPaper : MonoBehaviour
 {
+    private void Update()
+    {
+        HideIfClickedOutside(gameObject);
+    }
+
     public void CloseJournalPaper()
     {
         gameObject.SetActive(false);
+    }
+
+    private void HideIfClickedOutside(GameObject panel)
+    {
+        if (Input.GetMouseButton(0) && panel.activeSelf &&
+            !RectTransformUtility.RectangleContainsScreenPoint(
+                panel.GetComponent<RectTransform>(),
+                Input.mousePosition,
+                null))
+        {
+            
+            panel.SetActive(false);
+        }
     }
 }
